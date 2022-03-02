baf04e5c07f53475957f154ec69fc6533a31355c
Adam Rubin <arubin@carnegierobotics.com>
2022-03-02 17:00:56 -0500

    Add CI.

---
+++ src/greenhouse/schema_greenhouse_job_opening.go
@@ -0,0 +1,14 @@
+package greenhouse
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func schemaGreenhouseJobOpening() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"name": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+	}
+}
